a33e368315161c7ea76171c585dd4e02caef68c5
DGC_USER <dgc_user@DGC1103382m.local>
2025-11-20 21:11:05 +0800

    創建 gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/NewSiteTest/',
   server: {
     port: 5252, // 這裡改成你想要的 port
   },
