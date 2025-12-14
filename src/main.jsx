43c54ccf9362e5344b97ff7387fff694e6f09d6a
elmohsiao <elmohsiao@elmohsiaodeMac-mini.local>
2025-12-15 00:54:47 +0800

    Remove .DS_Store files && Add 404.html for SPA routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// 處理 404.html 的重定向
+const params = new URLSearchParams(window.location.search)
+const redirect = params.get('redirect')
+if (redirect) {
+  window.history.replaceState(null, null, redirect)
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
